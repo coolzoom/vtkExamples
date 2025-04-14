c001c67ef28faab324af3b0ad262f5c1492295ae
souwang324 <33944965+souwang324@users.noreply.github.com>
2025-04-14 21:10:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vtkExamples
+ vtk example based on CSharp and activiz package
---
+++ vtkExamples/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+using Kitware.VTK;
+
+namespace vtkExamples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.renderWindowControl = new Kitware.VTK.RenderWindowControl();
+            this.renderWindowControl.AddTestActors = false;
+            this.renderWindowControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.renderWindowControl.Location = new System.Drawing.Point(0, 0);
+            this.renderWindowControl.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.renderWindowControl.Name = "renderWindowControl";
+            this.renderWindowControl.Size = new System.Drawing.Size(617, 347);
+            this.renderWindowControl.TabIndex = 0;
+            this.renderWindowControl.TestText = null;
+            this.renderWindowControl.Load += new System.EventHandler(this.renderWindowControl1_Load);
+            pictureBox1.Controls.Add(this.renderWindowControl);
+        }
+
+        public Kitware.VTK.RenderWindowControl renderWindowControl;
+
+        private void renderWindowControl1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ReadPlainText();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK);
+            }
+        }
+
+
+        private void ReadPlainText()
+        {
+            // Path to vtk data must be set as an environment variable
+            // VTK_DATA_ROOT = "C:\VTK\vtkdata-5.8.0"
+            //vtkTesting test = vtkTesting.New();
+            //string root = test.GetDataRoot();
+            //string filePath = System.IO.Path.Combine(root, @"Data\teapot.xyz");
+            string filePath = @"D:\resource\vtkRes\VTKDataFiles-9.2.0.rc1\VTK-9.2.0.rc1\Testing\Data\TeapotPoints.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+            String sLineBuffer;
+            String[] sXYZ;
+            char[] chDelimiter = new char[] { ' ', '\t', ';' };
+            double[] xyz = new double[3];
+            vtkPoints points = vtkPoints.New();
+            int cnt = 0;
+
+            try
+            {
+                // in case file must be open in another application too use "FileShare.ReadWrite"
+                fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                sr = new StreamReader(fs);
+                while (!sr.EndOfStream)
+                {
+                    sLineBuffer = sr.ReadLine();
+                    cnt++;
+                    sXYZ = sLineBuffer.Split(chDelimiter, StringSplitOptions.RemoveEmptyEntries);
+                    if (sXYZ == null || sXYZ.Length != 3)
+                    {
+                        MessageBox.Show("data seems to be in wrong format at line " + cnt, "Format Exception", MessageBoxButtons.OK);
+                        return;
+                    }
+                    xyz[0] = double.Parse(sXYZ[0], CultureInfo.InvariantCulture);
+                    xyz[1] = double.Parse(sXYZ[1], CultureInfo.InvariantCulture);
+                    xyz[2] = double.Parse(sXYZ[2], CultureInfo.InvariantCulture);
+                    points.InsertNextPoint(xyz[0], xyz[1], xyz[2]);
+                }
+                vtkPolyData polydata = vtkPolyData.New();
+                polydata.SetPoints(points);
+                vtkVertexGlyphFilter glyphFilter = vtkVertexGlyphFilter.New();
+                glyphFilter.SetInputConnection(polydata.GetProducerPort());
+
+                // Visualize
+                vtkPolyDataMapper mapper = vtkPolyDataMapper.New();
+                mapper.SetInputConnection(glyphFilter.GetOutputPort());
+
+                vtkActor actor = vtkActor.New();
+                actor.SetMapper(mapper);
+                actor.GetProperty().SetPointSize(4);
+                actor.GetProperty().SetColor(1, 0.5, 0);
+                // get a reference to the renderwindow of our renderWindowControl1
+                vtkRenderWindow renderWindow = renderWindowControl.RenderWindow;
+                // renderer
+                vtkRenderer renderer = renderWindow.GetRenderers().GetFirstRenderer();
+                // set background color
+                renderer.SetBackground(0.2, 0.3, 0.4);
+                // add our actor to the renderer
+                renderer.AddActor(actor);
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "IOException", MessageBoxButtons.OK);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                    sr.Dispose();
+                    sr = null;
+                }
+            }
+        }
+
+    }
+}
