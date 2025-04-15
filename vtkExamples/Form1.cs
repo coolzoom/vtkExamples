6bd8553db4061ba1d1e28aa8eab9aa3df65d8d09
souwang324 <33944965+souwang324@users.noreply.github.com>
2025-04-15 21:11:00 +0900

    function  DistanceBetweenPoints added
    https://examples.vtk.org/site/CSharp/SimpleOperations/DistanceBetweenPoints/

--- vtkExamples/Form1.cs
+++ vtkExamples/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using System.Runtime.InteropServices;
 
 using Kitware.VTK;
 
@@ -35,11 +36,27 @@ namespace vtkExamples
 
         public Kitware.VTK.RenderWindowControl renderWindowControl;
 
+        public enum eExample {
+            eReadPlainText = 0,
+            eDistanceBetweenPoints
+        };
+
         private void renderWindowControl1_Load(object sender, EventArgs e)
         {
             try
             {
-                ReadPlainText();
+                eExample   ex = (eExample)1;
+                switch (ex) {
+                    case eExample.eReadPlainText:
+                        clsReadPlainText.ReadPlainText(renderWindowControl);
+                        break;
+                    case eExample.eDistanceBetweenPoints:
+                        DistanceBetweenPoints();
+                        break;
+
+                }
+
+               
             }
             catch (Exception ex)
             {
@@ -48,78 +65,28 @@ namespace vtkExamples
         }
 
 
-        private void ReadPlainText()
+        private void DistanceBetweenPoints()
         {
-            // Path to vtk data must be set as an environment variable
-            // VTK_DATA_ROOT = "C:\VTK\vtkdata-5.8.0"
-            //vtkTesting test = vtkTesting.New();
-            //string root = test.GetDataRoot();
-            //string filePath = System.IO.Path.Combine(root, @"Data\teapot.xyz");
-            string filePath = @"D:\resource\vtkRes\VTKDataFiles-9.2.0.rc1\VTK-9.2.0.rc1\Testing\Data\TeapotPoints.txt";
-            FileStream fs = null;
-            StreamReader sr = null;
-            String sLineBuffer;
-            String[] sXYZ;
-            char[] chDelimiter = new char[] { ' ', '\t', ';' };
-            double[] xyz = new double[3];
-            vtkPoints points = vtkPoints.New();
-            int cnt = 0;
+            // Create two points.
+            double[] p0 = new double[] { 0.0, 0.0, 0.0 };
+            double[] p1 = new double[] { 1.0, 1.0, 1.0 };
 
-            try
-            {
-                // in case file must be open in another application too use "FileShare.ReadWrite"
-                fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                sr = new StreamReader(fs);
-                while (!sr.EndOfStream)
-                {
-                    sLineBuffer = sr.ReadLine();
-                    cnt++;
-                    sXYZ = sLineBuffer.Split(chDelimiter, StringSplitOptions.RemoveEmptyEntries);
-                    if (sXYZ == null || sXYZ.Length != 3)
-                    {
-                        MessageBox.Show("data seems to be in wrong format at line " + cnt, "Format Exception", MessageBoxButtons.OK);
-                        return;
-                    }
-                    xyz[0] = double.Parse(sXYZ[0], CultureInfo.InvariantCulture);
-                    xyz[1] = double.Parse(sXYZ[1], CultureInfo.InvariantCulture);
-                    xyz[2] = double.Parse(sXYZ[2], CultureInfo.InvariantCulture);
-                    points.InsertNextPoint(xyz[0], xyz[1], xyz[2]);
-                }
-                vtkPolyData polydata = vtkPolyData.New();
-                polydata.SetPoints(points);
-                vtkVertexGlyphFilter glyphFilter = vtkVertexGlyphFilter.New();
-                glyphFilter.SetInputConnection(polydata.GetProducerPort());
+            IntPtr pP0 = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(double)) * 3);
+            IntPtr pP1 = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(double)) * 3);
+            Marshal.Copy(p0, 0, pP0, 3);
+            Marshal.Copy(p1, 0, pP1, 3);
 
-                // Visualize
-                vtkPolyDataMapper mapper = vtkPolyDataMapper.New();
-                mapper.SetInputConnection(glyphFilter.GetOutputPort());
+            // Find the squared distance between the points.
+            double squaredDistance = vtkMath.Distance2BetweenPoints(pP0, pP1);
 
-                vtkActor actor = vtkActor.New();
-                actor.SetMapper(mapper);
-                actor.GetProperty().SetPointSize(4);
-                actor.GetProperty().SetColor(1, 0.5, 0);
-                // get a reference to the renderwindow of our renderWindowControl1
-                vtkRenderWindow renderWindow = renderWindowControl.RenderWindow;
-                // renderer
-                vtkRenderer renderer = renderWindow.GetRenderers().GetFirstRenderer();
-                // set background color
-                renderer.SetBackground(0.2, 0.3, 0.4);
-                // add our actor to the renderer
-                renderer.AddActor(actor);
-            }
-            catch (IOException ex)
-            {
-                MessageBox.Show(ex.Message, "IOException", MessageBoxButtons.OK);
-            }
-            finally
-            {
-                if (sr != null)
-                {
-                    sr.Close();
-                    sr.Dispose();
-                    sr = null;
-                }
-            }
+            // Take the square root to get the Euclidean distance between the points.
+            double distance = Math.Sqrt(squaredDistance);
+
+            // Output the results.
+            Console.WriteLine("SquaredDistance = " + squaredDistance);
+            Console.WriteLine("Distance = " + distance);
+            Marshal.FreeHGlobal(pP0);
+            Marshal.FreeHGlobal(pP1);
         }
 
     }
